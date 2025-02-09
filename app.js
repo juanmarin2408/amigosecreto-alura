0299a715dc929149d68a684173e2adf6f6f160e2
juanmarin2408 <juan.marin13@usc.edu.co>
2025-02-09 18:28:06 -0500

    implementacion del registro de usuarios, y mostrar los usuarios en la pagina

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// crear array para almancenar los nombres de las personas
+let arrayNombrePersonas = [];
+
+//crear funcion para almacenar los nombres de las personas dentro del array
+function almacenarNombres(){
+    //verificar que el input no este vacio
+    if(document.getElementById('amigo').value === ''){
+        //en caso de estarlo mostrar un mensaje de alerta
+        alert("Ingresa el nombre de tu amigo");
+    }else{
+        //almacenar nombre del usuario dentro del array
+        arrayNombrePersonas.push(document.getElementById('amigo').value);
+        
+    }
+
+    //limpiar el input
+    document.getElementById('amigo').value = '';
+    mostrarUsuario()
+}
+
+//funcion para mostrar la lista de amigos dentro de la pagina
+function mostrarUsuario(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for(let i = 0; i < arrayNombrePersonas.length; i++){
+        let li = document.createElement('li');
+        li.textContent = arrayNombrePersonas[i];
+        lista.appendChild(li);
+    }
+}
