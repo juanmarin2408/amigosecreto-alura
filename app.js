41532a87abbc8aa43f76ff9314da8e0e48b1fbc4
juanmarin2408 <juan.marin13@usc.edu.co>
2025-02-09 18:38:37 -0500

    terminado el proyecto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // crear array para almancenar los nombres de las personas
 let arrayNombrePersonas = [];
-
+let lista = document.getElementById('listaAmigos');
 //crear funcion para almacenar los nombres de las personas dentro del array
 function almacenarNombres(){
     //verificar que el input no este vacio
@@ -21,11 +21,23 @@ function almacenarNombres(){
 
 //funcion para mostrar la lista de amigos dentro de la pagina
 function mostrarUsuario(){
-    let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
     for(let i = 0; i < arrayNombrePersonas.length; i++){
         let li = document.createElement('li');
         li.textContent = arrayNombrePersonas[i];
         lista.appendChild(li);
     }
+}
+
+function sortearAmigo(){
+    if(arrayNombrePersonas.length === 0 || arrayNombrePersonas.length === 1){
+        alert("No hay usuarios para sortear o solo hay un usuario");
+    }else{
+        let numeroUsuarios = arrayNombrePersonas.length;
+        let sorteoAleatorio = Math.floor(Math.random()*numeroUsuarios);
+        let amigoSeleccionado = arrayNombrePersonas[sorteoAleatorio];
+        lista.innerHTML = '';
+        lista.innerHTML = `El amigo ganador fue ${amigoSeleccionado}`;
+    }
+    
 }
